174a1bfcf800eeaa04ceb3a8c4bb701a0294fea1
Simondelali <atiegardelali@gmail.com>
2023-10-07 19:36:39 +0000

    create starter files

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,26 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+posts = [
+    {
+        'author': 'simon',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'August 28, 2023'
+    },
+    {
+        'author': 'Kofi',
+        'title': 'Blog post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2023'
+    }
+]
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title':'About'})
