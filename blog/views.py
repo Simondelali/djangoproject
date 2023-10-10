9aa9d39a6148a4efc780fb2217cdf145495259f9
Simondelali <atiegardelali@gmail.com>
2023-10-10 17:26:03 +0000

    set up user registration

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 
-posts = [
-    {
-        'author': 'simon',
-        'title': 'Blog post 1',
-        'content': 'First post content',
-        'date_posted': 'August 28, 2023'
-    },
-    {
-        'author': 'Kofi',
-        'title': 'Blog post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2023'
-    }
-]
-
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
